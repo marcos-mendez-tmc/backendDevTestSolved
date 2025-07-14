fcc392715fce3b4be15d8cf6d3b9bdaeb5bd0459
Marcos Mendez <marcos.mendez_ext@360padelgroup.com>
2025-07-14 12:23:26 +0200

    First Commit.

---
+++ src/main/java/com/markitos/backendDevTestSolved/BackendDevTestSolvedApplication.java
@@ -0,0 +1,13 @@
+package com.markitos.backendDevTestSolved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevTestSolvedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevTestSolvedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markitos/backendDevTestSolved/BackendDevTestSolvedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markitos.backendDevTestSolved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevTestSolvedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
